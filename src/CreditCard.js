b61873f917e4a488877445cae708dc8a18e4aaa9
Kevin Kipp <kevin.kipp@gmail.com>
2018-08-11 19:03:12 -0500

    Add credit card styles

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+import creditCards from "./creditCards.json";
+
 import "./CreditCard.scss";
 
+console.log(creditCards);
+
 const CreditCard = ({ type, number, name, expiration }) => (
-  <div className="CreditCard">
+  <div className="CreditCard" style={creditCards[type].styles}>
     <div className="CreditCard__type">{type}</div>
     <div className="CreditCard__name">{name}</div>
-    <div className="CreditCard__number">{number}</div>
+    <div className="CreditCard__number">
+      {number.map((n, i) => (
+        <span key={i} className="CreditCard__number-section">
+          {n}
+        </span>
+      ))}
+    </div>
     <div className="CreditCard__expiration">{expiration}</div>
   </div>
 );
